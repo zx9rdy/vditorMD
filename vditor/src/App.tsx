07a2c9ffc03a56da471bdc6535ed57bf64df56ed
zx9rdy <129975877+zx9rdy@users.noreply.github.com>
2023-06-09 18:39:07 +0800

    Add files via upload

---
+++ vditor/src/App.tsx
@@ -0,0 +1,18 @@
+import "vditor/dist/index.css";
+import React from "react";
+import Vditor from "vditor";
+
+const App = () => {
+  const [vd, setVd] = React.useState<Vditor>();
+  React.useEffect(() => {
+    const vditor = new Vditor("vditor", {
+      after: () => {
+        vditor.setValue("`Vditor` 最小代码示例");
+        setVd(vditor);
+      }
+    });
+  }, []);
+  return <div id="vditor" className="vditor" />;
+};
+
+export default App;
